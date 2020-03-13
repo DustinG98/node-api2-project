3f8028478dbdd23ceb3c625282db97fe97f30dae
DustinG98 <thedustingraham@gmail.com>
2020-03-12 20:27:05 -0600

    initial client

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 
 const server = express();
 
 const postsRouter = require('./routes/postsRouter')
 
 server.use(express.json())
+
+server.use(cors())
 
 server.use('/api/posts', postsRouter)
 
