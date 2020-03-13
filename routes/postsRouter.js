6e045078d5d44f347e90f7ae15f5082b51502d9c
DustinG98 <thedustingraham@gmail.com>
2020-03-12 20:15:12 -0600

    all get requests, delete by id, update by id

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -71,7 +71,68 @@ router.route('/:id').get((req, res) => {
         })
 })
 
+//GET POSTS COMMENTS
+router.route('/:id/comments').get((req, res) => {
+    database.findById(req.params.id)
+        .then(post => {
+            if(post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+                return;
+            }
+            database.findPostComments(req.params.id)
+                .then(comments => {
+                    res.status(200).json(comments)
+                })
+                .catch(() => {
+                    res.status(500).json({error: "The comments information could not be retreived."})
+                })
+        })
+})
 
+//DELETE POST BY ID
+router.route('/:id').delete((req, res) => {
+    database.findById(req.params.id)
+        .then(post => {
+            if(post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+                return;
+            }
+            database.remove(req.params.id)
+                .then(() => {
+                    res.status(200).json({ message: "Post has been deleted" })
+                })
+                .catch(() => {
+                    res.status(500).json({ error: "the post could not be removed" })
+                })
+        })
+})
+
+//UPDATE POST BY ID
+router.route('/:id').put((req, res) => {
+    database.findById(req.params.id)
+        .then(post => {
+            if(post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+                return;
+            }
+            if(!req.body.title || !req.body.contents) {
+                res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+            }
+            database.update(req.params.id, req.body)
+                .then(count => {
+                    if(count === 1) {
+                        database.findById(req.params.id)
+                            .then(post => {
+                                res.status(200).json(post[0])
+                            })
+                    } else if(count === 0) {
+                        res.status(500).json({ error: "The post information could not be modified." })
+                        return;
+                    }
+
+                })
+        })
+})
 
 module.exports = router;
 
